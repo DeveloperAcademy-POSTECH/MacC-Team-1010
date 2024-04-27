565ed5fac6bae0f4b7c60cd64d67a8e652225b40
sunghun <leedool3003@gmail.com>
2024-04-27 16:35:37 +0900

    :hammer: [chore] 각 모듈 마다 Tests 파일 추가 #52

---
+++ Projects/App/Tests/AppTests.swift
@@ -0,0 +1,8 @@
+//
+//  AppTests.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Kim SungHun on 4/27/24.
+//
+
+import Foundation
---
+++ Projects/Core/Tests/CoreTests.swift
@@ -0,0 +1,8 @@
+//
+//  CoreTests.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Kim SungHun on 4/27/24.
+//
+
+import Foundation
